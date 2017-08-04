dd1fa243c7aa7fd59fcfb8fe9b4e8cb0b51d40bf
hiccup01 <hiccup01@hiccup01.com>
2017-08-04 21:14:13 +1200

    A load of stuff

---
+++ ChangeButton.js
@@ -0,0 +1,218 @@
+var ChangeButton = (function () { 'use strict';
+
+function encapsulateStyles ( node ) {
+	setAttribute( node, 'svelte-3680194763', '' );
+}
+
+function add_css () {
+	var style = createElement( 'style' );
+	style.id = 'svelte-3680194763-style';
+	style.textContent = "[svelte-3680194763]#nextbutton,[svelte-3680194763] #nextbutton{height:50px;width:50px;border-radius:20px;display:flex}";
+	appendNode( style, document.head );
+}
+
+function create_main_fragment ( state, component ) {
+	var button;
+
+	return {
+		create: function () {
+			button = createElement( 'button' );
+			this.hydrate();
+		},
+
+		hydrate: function ( nodes ) {
+			encapsulateStyles( button );
+			button.id = "nextbutton";
+		},
+
+		mount: function ( target, anchor ) {
+			insertNode( button, target, anchor );
+		},
+
+		unmount: function () {
+			detachNode( button );
+		},
+
+		destroy: noop
+	};
+}
+
+function ChangeButton ( options ) {
+	options = options || {};
+	this._state = options.data || {};
+
+	this._observers = {
+		pre: Object.create( null ),
+		post: Object.create( null )
+	};
+
+	this._handlers = Object.create( null );
+
+	this._root = options._root || this;
+	this._yield = options._yield;
+
+	this._torndown = false;
+	if ( !document.getElementById( 'svelte-3680194763-style' ) ) add_css();
+
+	this._fragment = create_main_fragment( this._state, this );
+
+	if ( options.target ) {
+		this._fragment.create();
+		this._fragment.mount( options.target, null );
+	}
+}
+
+assign( ChangeButton.prototype, {
+ 	get: get,
+ 	fire: fire,
+ 	observe: observe,
+ 	on: on,
+ 	set: set
+ });
+
+ChangeButton.prototype._set = function _set ( newState ) {
+	var oldState = this._state;
+	this._state = assign( {}, oldState, newState );
+	dispatchObservers( this, this._observers.pre, newState, oldState );
+	dispatchObservers( this, this._observers.post, newState, oldState );
+};
+
+ChangeButton.prototype.teardown = ChangeButton.prototype.destroy = function destroy ( detach ) {
+	this.fire( 'destroy' );
+
+	if ( detach !== false ) this._fragment.unmount();
+	this._fragment.destroy();
+	this._fragment = null;
+
+	this._state = {};
+	this._torndown = true;
+};
+
+function setAttribute(node, attribute, value) {
+	node.setAttribute(attribute, value);
+}
+
+function createElement(name) {
+	return document.createElement(name);
+}
+
+function appendNode(node, target) {
+	target.appendChild(node);
+}
+
+function insertNode(node, target, anchor) {
+	target.insertBefore(node, anchor);
+}
+
+function detachNode(node) {
+	node.parentNode.removeChild(node);
+}
+
+function noop() {}
+
+function assign(target) {
+	var k,
+		source,
+		i = 1,
+		len = arguments.length;
+	for (; i < len; i++) {
+		source = arguments[i];
+		for (k in source) target[k] = source[k];
+	}
+
+	return target;
+}
+
+function get(key) {
+	return key ? this._state[key] : this._state;
+}
+
+function fire(eventName, data) {
+	var handlers =
+		eventName in this._handlers && this._handlers[eventName].slice();
+	if (!handlers) return;
+
+	for (var i = 0; i < handlers.length; i += 1) {
+		handlers[i].call(this, data);
+	}
+}
+
+function observe(key, callback, options) {
+	var group = options && options.defer
+		? this._observers.post
+		: this._observers.pre;
+
+	(group[key] || (group[key] = [])).push(callback);
+
+	if (!options || options.init !== false) {
+		callback.__calling = true;
+		callback.call(this, this._state[key]);
+		callback.__calling = false;
+	}
+
+	return {
+		cancel: function() {
+			var index = group[key].indexOf(callback);
+			if (~index) group[key].splice(index, 1);
+		}
+	};
+}
+
+function on(eventName, handler) {
+	if (eventName === 'teardown') return this.on('destroy', handler);
+
+	var handlers = this._handlers[eventName] || (this._handlers[eventName] = []);
+	handlers.push(handler);
+
+	return {
+		cancel: function() {
+			var index = handlers.indexOf(handler);
+			if (~index) handlers.splice(index, 1);
+		}
+	};
+}
+
+function set(newState) {
+	this._set(assign({}, newState));
+	if (this._root._lock) return;
+	this._root._lock = true;
+	callAll(this._root._beforecreate);
+	callAll(this._root._oncreate);
+	callAll(this._root._aftercreate);
+	this._root._lock = false;
+}
+
+function dispatchObservers(component, group, newState, oldState) {
+	for (var key in group) {
+		if (!(key in newState)) continue;
+
+		var newValue = newState[key];
+		var oldValue = oldState[key];
+
+		if (differs(newValue, oldValue)) {
+			var callbacks = group[key];
+			if (!callbacks) continue;
+
+			for (var i = 0; i < callbacks.length; i += 1) {
+				var callback = callbacks[i];
+				if (callback.__calling) continue;
+
+				callback.__calling = true;
+				callback.call(component, newValue, oldValue);
+				callback.__calling = false;
+			}
+		}
+	}
+}
+
+function callAll(fns) {
+	while (fns && fns.length) fns.pop()();
+}
+
+function differs(a, b) {
+	return a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+}
+
+return ChangeButton;
+
+}());
--- page.js
+++ page.js
@@ -1,7 +1,1 @@
-var Button = require("./ChangeButton.svelte");
-var nextButton = new ChangeButton({
-	target: document.querySelector('thingy'),
-	data: {
-	}
-});
 console.log("test");
